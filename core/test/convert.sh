d40c49cbb1d2df1be4f183fc90c9379cf5b9cbaa
Oscar Bezi <oscar@bezi.io>
2015-08-08 14:33:56 -0700

    Updated scripts.

--- core/test/convert.sh
+++ core/test/convert.sh
@@ -1,21 +1,24 @@
 # @file convert.sh
-# @brief Converts all of the files in this directory starting with 'sample' to PDF.
+# @brief Converts all files in SRC_DIR to pdfs and stores them in DEST_DIR.
 # @since 8 August 2015
 # @author Oscar Bezi, bezi@scottylabs.org
 
 # Makes the script terminate on an error code.
 set -e;
+set -x;
 
-DIR="output"
-FILES=$(ls sample*);
+SRC_DIR="sample-files"
+DEST_DIR="output"
+FILES=$(ls $SRC_DIR/*);
 
-rm -rf $DIR;
-mkdir -p $DIR;
+# cleanup
+mkdir -p $DEST_DIR;
+rm -f $DEST_DIR/*.pdf;
 
 for i in $FILES;
 do
     echo "Processing $i";
-    soffice --headless --convert-to pdf $i --outdir $DIR > /dev/null 2>&1;
+    soffice --headless --convert-to pdf $i --outdir $DEST_DIR > /dev/null 2>&1;
 done
 
 echo "Success!";
--- core/test/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# @file test.sh
-# @brief A curl command to test the delivery endpoint.
-# @author Oscar Bezi, bezi@scottylabs.com
-# @since 8 August 2015
-
-# There are several test files.
-FILES=$(ls sample-files/sample*);
