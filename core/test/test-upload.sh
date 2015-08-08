792c6da22762e4ecf16d2d0bcf1d742d87d0529c
Oscar Bezi <oscar@bezi.io>
2015-08-08 16:26:45 -0700

    Added file to test the uploading of files.

---
+++ core/test/test-upload.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# @file test-upload.sh
+# @brief Uploads all of the sample files to the server under a few AndrewIDs.
+# @since 8 August 2015
+# @author Oscar Bezi, bezi@scottylabs.org
+
+# Makes the script terminate on an error code.
+set -e;
+
+SRC_DIR="sample-files"
+DEST_DIR="output"
+FILES=$(ls $SRC_DIR/*);
+
+ANDREW_IDS="odb jrgallag test"
+
+for id in $ANDREW_IDS;
+do
+    for file in $FILES;
+    do
+        curl -i -F "toPrint=@$file" http://localhost:8080/upload/$id
+    done
+done
