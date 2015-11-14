48c41f69a21cabf1fe049a047431b7814546eb41
hariank <hariank3k@gmail.com>
2015-11-14 16:12:10 -0500

    working gdrive uploads

--- web/app.js
+++ web/app.js
@@ -49,27 +49,42 @@ function createPicker() {
 function pickerCallback(data) {
     if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
         var doc = data[google.picker.Response.DOCUMENTS][0];
-        var doc_url = doc[google.picker.Document.URL];
 
-        console.log(doc);
+        var fileURL = "https://www.googleapis.com/drive/v2/files/" + doc.id.toString();
 
-        var url = "https://www.googleapis.com/drive/v2/files/" + doc.id.toString();
-
+        // first get file info and download link
         var xhr = new XMLHttpRequest();
         var accessToken = gapi.auth.getToken().access_token;
-        xhr.open("GET", url);
+        xhr.open("GET", fileURL);
         xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
         xhr.responseType = "json";
         xhr.onload = function()
         {
-            var blob = xhr.response;
-            console.log(blob);
+            var res = xhr.response;
+            var url;
+            if (res.downloadUrl)
+                url = res.downloadUrl;
+            else
+                url = res.exportLinks['application/pdf'];
+
+            // get file
+            var xhr2 = new XMLHttpRequest();
+            xhr2.open("GET", url);
+            xhr2.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+            xhr2.responseType = "blob";
+            xhr2.onload = function()
+            {
+                var blob = xhr2.response;
+                var f = new File([blob], res.title);
+                console.log(f);
+                postFile(f);
+            }
+            xhr2.send();
         }
         xhr.send();
     }
 }
 
-// dropbox upload
 
 function postFile(file) {
     var formData = new FormData();
@@ -80,6 +95,7 @@ function postFile(file) {
     request.send(formData);
 }
 
+// dropbox upload
 options = {
     success: function(files) {
         var url = files[0].link;
@@ -106,7 +122,6 @@ function dboxBtnClick() {
 }
 
 // direct/drag-and-drop file upload
-
 Dropzone.options.printDropzone = {
     paramName: "toPrint",
     init: function()  {
