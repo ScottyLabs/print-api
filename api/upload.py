cd7107b86aa03f0b729a4bdd51493523e756f2f9
Maxwell Johnson <maximusjohnsonus@gmail.com>
2016-10-15 14:37:25 -0400

    Removed last vestiges of saving files

--- api/upload.py
+++ api/upload.py
@@ -7,9 +7,6 @@ from subprocess import call
 
 ALLOWED_EXTENSIONS = set(['pdf']) #add more types! lp can probably deal with it
 
-#app = Flask(__name__)
-#app.secret_key = 'flawless'
-
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -20,24 +17,20 @@ def upload():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            #return redirect(request.url)
-            return 'no file part'
+            print('no file part in post request')
+            return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
-            #return redirect(request.url)
-            return 'no selected file'
+            print('no file selected')
+            return redirect(request.url)
         if not allowed_file(file.filename):
-            #return redirect(request.url)
-            return 'bad file type'
+            print('bad file type: ' + file.filename)
+            return redirect(request.url)
         if file:
-            filename = secure_filename(file.filename)
-            # if you want to save it:
-            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #return redirect(url_for('uploaded_file', filename=filename))
             # calling lp is something like this, but the pipe doesn't work
-            #call(["lp","-t","lp str txt"],stdin=file.stream());
+            #call(["lp","-t","lp str txt"],stdin=file.stream())
             return redirect('/')
     return '''
     <!doctype html>
@@ -48,8 +41,3 @@ def upload():
          <input type=submit value=Upload>
     </form>
     '''
-
-# part of saving the file
-#@app.route('/uploads/<filename>')
-#def uploaded_file(filename):
-#    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
